b8b707af666f2dc2ed8360a1dfc7670374603387
albertoNilWisdom <94020926+albertoNilWisdom@users.noreply.github.com>
2022-07-31 12:56:56 +0100

    Update housePricePredictor_extractCSV.py

--- housePricePredictor_extractCSV.py
+++ housePricePredictor_extractCSV.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import csv
 import re
-
-
+from prophet import Prophet
+#http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv
 print("WELCOME----")
 print("Enter first section of postcode e.g. GU46, or the full post code, with a space.")
 print("-----------")
@@ -34,7 +34,7 @@ def search(regex):
                      'unk1':[], 
                      'unk2':[]})
 
-    csvFile = '/home/ted/Documents/Programming/datasets/gov-paid-prices-housing/pp-complete.csv'
+    csvFile = '/home/ted/Documents/programming/datasets/gov-paid-prices-housing/pp-complete.csv'
     results = 0
 
     with open(csvFile) as f:
@@ -48,13 +48,22 @@ def search(regex):
 
     if results > 0:
         print(str(results) + ' total results, output file')
-        df.to_csv(r'/home/ted/Documents/Programming/datasets/gov-paid-prices-housing/currentSearch.csv', index = False)
+        df.rename(columns={'date':'ds','price':'y'}, inplace = True)
+        df['ds'] = pd.to_datetime(df['ds']).dt.date
+        df=df[['ds','y']]
+        df.to_csv(r'/home/ted/Documents/programming/datasets/gov-paid-prices-housing/currentSearch.csv', index = False)
     else:
         print('there are no results.',end="\r")
 
-search(regex)
+def prediction():
+    df = pd.read_csv('/home/ted/Documents/programming/datasets/gov-paid-prices-housing/currentSearch.csv')
+    df.head
+    m = Prophet(seasonality_mode ='multiplicative',mcmc_samples=300).fit(df, show_progress=True)
+    future = m.make_future_dataframe(periods=365)
+    fcst = m.predict(future)
+    fig = m.plot(fcst)
+    fcst.to_csv('predict_output.csv')
 
-print("WELCOME----")
-print("would you like to predict the future? (y/N)")
-print("-----------")
-postcode = input()
+search(regex)
+prediction()
+
